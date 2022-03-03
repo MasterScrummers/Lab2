b8770af54a7c50e1ef0ef4632a95ab2cb4a1fcac
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-03-03 21:31:19 +1100

    Audio Implementation

---
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    /// <summary>
+    /// Sound effects, music, does not matter as you call the clip via its name.
+    /// </summary>
+    public AudioClip[] audioClips;
+    private Dictionary<string, AudioClip> allClips;
+
+    private AudioSource musicPlayer;
+    private List<AudioSource> soundPlayers;
+
+    private void Awake()
+    {
+        allClips = new Dictionary<string, AudioClip>();
+        
+        foreach (AudioClip clip in audioClips)
+        {
+            if (!clip)
+            {
+                continue;
+            }
+            if (allClips.ContainsKey(clip.name))
+            {
+                throw new System.Exception("There are two clips with the name: '" + clip.name + "'");
+            }
+            allClips.Add(clip.name, clip);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundPlayers = new List<AudioSource>();
+        musicPlayer = gameObject.AddComponent<AudioSource>();
+        musicPlayer.loop = true;
+    }
+
+    private void PlayAudio(AudioSource source, string clipName)
+    {
+        if (!allClips.ContainsKey(clipName))
+        {
+            Debug.Log("The audioclip '" + clipName + "' does not exist." );
+            return;
+        }
+
+        if (source)
+        {
+            source.Stop();
+            source.clip = allClips[clipName];
+            source.Play();
+        }
+    }
+
+    public void PlayMusic(string clipName)
+    {
+        if (!musicPlayer.clip || !musicPlayer.clip.name.Equals(clipName))
+        {
+            PlayAudio(musicPlayer, clipName);
+        }
+    }
+
+    public void PlaySound(string clipName)
+    {
+        AudioSource player = null;
+        foreach (AudioSource source in soundPlayers)
+        {
+            if (source && !source.isPlaying)
+            {
+                player = source;
+            }
+        }
+
+        if (player == null)
+        {
+            player = gameObject.AddComponent<AudioSource>();
+            player.loop = false;
+            soundPlayers.Add(player);
+        }
+
+        PlayAudio(player, clipName);
+    }
+}
--- Assets/Scripts/Controllers/ControllerPack.cs
+++ Assets/Scripts/Controllers/ControllerPack.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Indestructable))]
 [RequireComponent(typeof(VariableController))]
+[RequireComponent(typeof(AudioController))]
 public class ControllerPack : MonoBehaviour
 {
     /* This script does nothing by ensures
