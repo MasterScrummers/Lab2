07e8ebd5515119db18abf6fb836bb158428d73bf
Ashietta <alyssaliang13@gmail.com>
2022-03-05 15:00:13 +1100

    Completed death animation

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    Rigidbody2D playerRigidbody2D;
+
+    const float deathLength = 5.0f;
+    float deathTimer;
+
+    bool deathSeq;
+    
+    void Awake() 
+    {
+        playerRigidbody2D = player.GetComponent<Rigidbody2D>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        deathSeq = false;
+        deathTimer = deathLength;
+        TriggerDeath(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (deathSeq) {
+            deathTimer -= Time.deltaTime;
+
+            if (deathTimer <= 0) {
+                TriggerRespawn();
+            }
+        }
+    }
+
+    public void TriggerDeath(bool playDeathAnim) {
+        
+        deathSeq = true;
+        
+        if (playDeathAnim)
+        {
+            StartDeathAnim();
+        }
+    }
+
+    void StartDeathAnim() {
+        playerRigidbody2D.AddForce(new Vector2(0f, 400));
+    }
+
+    public void TriggerRespawn() {
+
+        deathSeq = false;
+    }
+}
