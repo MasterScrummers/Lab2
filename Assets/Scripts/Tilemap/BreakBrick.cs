79649ca9836e9c3f86b32133c80fbfda9f150ce3
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-03-09 19:48:36 +1100

    Merge recovery

--- Assets/Scripts/Tilemap/BreakBrick.cs
+++ Assets/Scripts/Tilemap/BreakBrick.cs
@@ -1,31 +1,21 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-[RequireComponent(typeof(Tilemap))]
-public class BreakBrick : MonoBehaviour
+public class BreakBrick : BlockBase
 {
-    private Tilemap tilemap;
     private MarioSpriteUpdator mario;
-    // Start is called before the first frame update
-    void Start()
+
+    protected override void Start()
     {
-        tilemap = transform.GetComponent<Tilemap>();
+        base.Start();
         mario = DoStatic.GetPlayer().GetComponent<MarioSpriteUpdator>();
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    protected override void Effect(Vector3Int tilePos)
     {
-        //To check whether collisions occurs with Mario's head (i.e only trigger coin from underneath block)
-        if (col.collider.tag == "head")
+        if (mario.PowerState != 0)
         {
-            if (mario.PowerState == 0)
-            {
-                return;
-            }
-            //ADD IF STATEMENT TO CHECK WHETHER PLAYER IS BIG OR SMALL MARIO
-            Vector3Int cellPosition = tilemap.WorldToCell(col.GetContact(0).collider.transform.position);
-            cellPosition.y++;
-            tilemap.SetTile(cellPosition, null);
+            tilemap.SetTile(tilePos, null);
+            audioController.PlaySound("Brick Break");
         }
     }
 }
